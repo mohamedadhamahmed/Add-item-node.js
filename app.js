7737a6f31ed165f31e0993c3c0318f13e7634131
mohamedadhamismail <mohamedadhamismail0123456@gmail.com>
2021-03-25 01:47:25 +0200

    frist test

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const app=express();
 
 const mongoose=require('mongoose');
 var bodyParser = require('body-parser');
-const cors=require('cors');
-var port=process.env.PORT ||8080;
+ //const cors=require('cors');
 const productRoute=require('./route/products');
 mongoose.connect('mongodb+srv://mohamed:adhammedo3334444@cluster0.hbypk.mongodb.net/mohamed?retryWrites=true&w=majority',
 {
@@ -13,20 +12,22 @@ mongoose.connect('mongodb+srv://mohamed:adhammedo3334444@cluster0.hbypk.mongodb.
 });
 
 const connection=mongoose.connection;
+console.log("Database_URL", process.env.DATABASE_URL);
 
 connection.on('connected',()=>{console.log('connected with server')});
 connection.on('error',()=>{console.log('error with server')});
 
 app.use([bodyParser.urlencoded({extended:true})
     ,express.json()])
-app.use(cors());
+//app.use(cors());
   app.use('/products',productRoute);
 // app.post('/products',function (req, res) {
 //     var url = req.body;
 //     console.log(url)
 // });
+const PORT=process.env.PORT ||8080;
 
-app.listen(port,()=>{
-    console.log("server sarted");
+app.listen(PORT,()=>{
+    console.log("server sarted"+PORT);
 })
 module.exports=app
